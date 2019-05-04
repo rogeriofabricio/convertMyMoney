59ac0ee23837aac74844601b71b842855c7fab4a
rogeriofabricio <rogerio.fabricio@hotmail.com>
2019-05-04 19:05:38 -0300

    now 1.1
    now 1.1

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const convert = require('./lib/convert')
 
 const port = process.env.PORT || 3000
 
-app.set('view engine', 'ejs')
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('lib', path.join(__dirname, 'lib'))
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
