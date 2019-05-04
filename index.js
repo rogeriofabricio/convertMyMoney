2785a8d6b9b2725b863fc4fbf1b5cb8c97756083
rogeriofabricio <rogerio.fabricio@hotmail.com>
2019-05-04 18:54:49 -0300

    adding lib
    adding lib

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.set('lib', path.join(__dirname, 'lib'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
